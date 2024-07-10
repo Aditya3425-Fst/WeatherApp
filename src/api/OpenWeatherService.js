1800924343c8cd950d98edaba37bafd748cab8b3
Aditya kumar Tiwari <sujanian3425@gmail.com>
2024-07-10 19:59:49 +0530

    weather

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = '48539b8c0c0d544116a1e09da0058812';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
